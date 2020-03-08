180d918e7073988abf94c3d953859b75dd5a058b
Troy Walther <tdgames29@gmail.com>
2020-03-07 21:57:01 -0500

    Made Employee more verbose

--- src/com/company/Employee.java
+++ src/com/company/Employee.java
@@ -10,6 +10,21 @@ public class Employee {
     private List<Shift> shifts; //maybe only store shifts in sql, no need to store in program
     private float wage;
     static private List<OrderRequest> orders; //Shared among all employees
-
-
+    
+    //For manager to view and change shifts
+	public List<Shift> getShifts() {
+		return shifts;
+	}
+	public void setShifts(List<Shift> shifts) {
+		this.shifts = shifts;
+	}
+	
+	//For manager to set wage
+	public float getWage() {
+		return wage;
+	}
+	public void setWage(float wage) {
+		this.wage = wage;
+	}
+    
 }
--- src/com/company/Housekeeping.java
+++ src/com/company/Housekeeping.java
@@ -1,11 +1,11 @@
 package com.company;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class Housekeeping extends Employee {
 
     //Shared among all kitchen staff
-    static private List<CleaningTicket> activeTickets;
+    static private ArrayList<CleaningTicket> activeTickets;
 
     //Move ticket from active to complete
     public void CompleteTicket()
