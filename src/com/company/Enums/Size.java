80f3b49df06f1d130b09ea719a33388e6b661fc9
connoralittle <57184344+connoralittle@users.noreply.github.com>
2020-03-07 19:50:12 -0500

    added the state enum

---
+++ src/com/company/Enums/Size.java
@@ -0,0 +1,10 @@
+package com.company.Enums;
+
+public enum Size {
+	DELUXE, 	//smaller Nicer view or Ammenities often King can get single bed
+	STANDARD, 	//2 Queen
+	KING, 		//king bed
+	SUITE,		//Has balcony
+	PRESSUITE, 	//Most expensive Sweet
+	CONNECTING, //useful for disabilities
+}
--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -3,47 +3,52 @@ package com.company;
 import java.util.Date;
 import java.util.List;
 
+import com.company.Enums.*;
+
 public class Room {
-    private int ID;
-    private int beds;   //number of beds
-    private int size; //Size scaled from 0 to something or other
-    private boolean reserved;
-    private boolean occupied;
-    private String state;   //May use this instead of booleans for reserved or occupied
-    private Customer client;
-    private Date checkoutDate;
-    private List<Date> futureBookings;
-    private RoomServiceTicket roomService;
+	private int ID;
+	private int beds; // number of beds
+	private Size size; // Size scaled from 0 to something or other
+	private View view;
+	private int floor;
+	private State state; // May use this instead of booleans for reserved or occupied
+	private Customer client;
+	private Date checkoutDate;
+	private List<Date> futureBookings;
+	private RoomServiceTicket roomService;
 
-    public Room()
+	public Room(int ID, int beds, Size size, View view, int floor)
     {
-    	
+    	this.ID = ID;
+    	this.beds = beds;
+    	this.size = size;
+    	this.view = view;
+    	this.floor = floor;
+    	this.state = State.CLEAN;
+    	this.client = null;
+    	this.checkoutDate = null;
+    	this.futureBookings = null;
+    	this.roomService = null;
     }
+	
+	public void checkIn(Customer client) {
+		this.client = client;
+	}
 
-    public void addBed()
-    {
+	public void reserve() {
 
-    }
+	}
 
-    public void reserve()
-    {
+	public List<Date> checkListings() {
+		return futureBookings;
+	}
 
-    }
+	public Customer checkClient() {
+		return client;
+	}
 
-    public List<Date> checkListings()
-    {
-        return futureBookings;
-    }
+	public void checkout() {
 
-    public Customer checkClient()
-    {
-        return client;
-    }
-
-    public void checkout()
-    {
-
-    }
-
+	}
 
 }
